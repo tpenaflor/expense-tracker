feb2ce3facb3e1df6c62db561a483c96e1e53251
Tyrone Penaflor <penaflor.tyrone@gmail.com>
2021-01-25 21:00:31 +0800

    heroku mush deploy

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,6 @@ import {NavLink} from 'react-router-dom'
 const Header = () => (
     <header>
         <h1>Expensify Header</h1>
-        <h1>I'm in heroku</h1>
         <NavLink activeClassName="is-active" to='/' exact={true}>Home</NavLink> 
         <NavLink activeClassName="is-active" to='/createExpense'>Create</NavLink> 
         <NavLink activeClassName="is-active" to='/help'>Help</NavLink> 
