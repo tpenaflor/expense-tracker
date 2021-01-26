7993cbd9b2f161ee251541309783b70afc06dd56
Tyrone Penaflor <penaflor.tyrone@gmail.com>
2021-01-26 09:57:11 +0800

    new amount format

--- src/components/ExpenseEntry.js
+++ src/components/ExpenseEntry.js
@@ -2,11 +2,12 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 import moment from 'moment'
+
 const ExpenseEntry = ({id, desc, amount, createAt}) => (
     <div> 
         <h2>
         <Link to={`/editExpense/${id}`} >{desc}</Link></h2>
-        <p>{`${(amount/100).toFixed(2)} on ${moment(createAt).format("MMM DD, YYYY")}`}</p>
+        <p>{`${Intl.NumberFormat('en-US',{style:'currency',currency:'USD'}).format(amount/100)} on ${moment(createAt).format("MMM DD, YYYY")}`}</p>
     </div>
 )
 
